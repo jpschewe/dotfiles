f6dfd02f355d087819a3788eda5c3c38c9e50d18
Jon Schewe <jpschewe@mtu.net>
2004-05-13 12:19:28 +0000

    *** empty log message ***

--- custom.el
+++ custom.el
@@ -1,2 +1,10 @@
 ;;(custom-set-faces
 ;; '(default ((t (:size "12pt" :family "Courier"))) t))
+(custom-set-variables
+'(load-home-init-file t t)
+'(package-get-remote (quote (("ftp.xemacs.org" "/pub/xemacs/packages"))))
+'(canlock-password "cd819716e1c18b4c05b3a874555251efbdc65290")
+'(gutter-buffers-tab-visible nil)
+'(gutter-buffers-tab-enabled nil)
+)
+(custom-set-faces)
