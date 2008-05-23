079e86a075e52454f8af667033ef0eb94b84641b
Jon Schewe <jpschewe@mtu.net>
2008-05-23 17:51:29 +0000

    Changes for Mac setup

--- setup.sh
+++ setup.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 
-mypath="`echo $0 | /bin/sed -e 's,[^/]*$,,;s,/$,,;s,^$,.,'`"
+mypath="`echo $0 | sed -e 's,[^/]*$,,;s,/$,,;s,^$,.,'`"
 cd ${mypath}
 mypath=$PWD
 
-/bin/ln -sf "${mypath}/addpath" "${HOME}/.addpath"
-/bin/ln -sf "${mypath}/bash_login" "${HOME}/.bash_login"
-/bin/ln -sf "${mypath}/bash_logout" "${HOME}/.bash_logout"
-/bin/ln -sf "${mypath}/bashrc" "${HOME}/.bashrc"
-/bin/ln -sf "${mypath}/packages" "${HOME}/.packages"
-/bin/ln -sf "${mypath}/profile" "${HOME}/.profile"
+ln -sf "${mypath}/addpath" "${HOME}/.addpath"
+ln -sf "${mypath}/bash_login" "${HOME}/.bash_login"
+ln -sf "${mypath}/bash_logout" "${HOME}/.bash_logout"
+ln -sf "${mypath}/bashrc" "${HOME}/.bashrc"
+ln -sf "${mypath}/packages" "${HOME}/.packages"
+ln -sf "${mypath}/profile" "${HOME}/.profile"
 
 # ask about ssh
 echo -n "Would you like to have ssh-agent start up on login? (y/N) "
 answer=''
 read answer
 if [ "x${answer}" = "xy" ]; then
-  /bin/mkdir -p "${HOME}/.ssh"
-  /bin/ln -sf "${mypath}/sssha" "${HOME}/.ssh/sssha"
+  mkdir -p "${HOME}/.ssh"
+  ln -sf "${mypath}/sssha" "${HOME}/.ssh/sssha"
 else
-  /bin/rm -f "${HOME}/.ssh/sssha"
+  rm -f "${HOME}/.ssh/sssha"
 fi
 
 # subversion
-/bin/mkdir -p "${HOME}/.subversion"
+mkdir -p "${HOME}/.subversion"
 if [ -f "${HOME}/.subversion/config" ]; then
   /bin/mv "${HOME}/.subversion/config" "${HOME}/.subversion/config.old"
 fi
-/bin/ln -fs "${mypath}/subversion-config" "${HOME}/.subversion/config"
+ln -fs "${mypath}/subversion-config" "${HOME}/.subversion/config"
