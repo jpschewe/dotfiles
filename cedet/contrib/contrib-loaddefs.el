05a835960c4c76c51026359d0b0c80ab42bfce73
Jon Schewe <jpschewe@mtu.net>
2008-10-04 01:53:20 +0000

    Cleanup

--- cedet/contrib/contrib-loaddefs.el
+++
@@ -1,13 +0,0 @@
-;;; DO NOT MODIFY THIS FILE
-
-;;; contrib-loaddefs.el --- Auto-generated CEDET autoloads
-
-;;;### (autoloads (wisent-csharp-default-setup) "wisent-csharp" "contrib/wisent-csharp.el")
-
-(autoload 'wisent-csharp-default-setup "wisent-csharp" nil nil nil)
-
-(add-hook 'csharp-mode-hook (function wisent-csharp-default-setup))
-
-;;;***
-
-
