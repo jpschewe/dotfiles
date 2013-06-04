87b52621bbbd7b2d34a6156452860aa7b82a9872
Jon Schewe <jpschewe@mtu.net>
2013-06-03 20:40:03 -0500

    Ignore .git when doing recursive grep

--- init.el
+++ init.el
@@ -420,7 +420,7 @@
 (message "grep")
 ;;change the default to be my perl script
 ;;(setq grep-command "grep -n -R --exclude='*CVS*' --exclude='*.svn*' ")
-(setq grep-find-command "find . \\( -name .svn -prune \\) -o \\( -name CVS -prune \\) -o -type f -print0 | xargs -0 grep -n ")
+(setq grep-find-command "find . \\( -name .git -prune \\) -o \\( -name .svn -prune \\) -o \\( -name CVS -prune \\) -o -type f -print0 | xargs -0 grep -n ")
 
 
 ;;;;;;;;;;;
