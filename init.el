0d54c7fed247396bd3a1b30146665d8bc22ea8dc
Jon Schewe <jpschewe@mtu.net>
2017-03-08 14:14:12 -0600

    Use github flavored markdown for md files

--- init.el
+++ init.el
@@ -1934,7 +1934,7 @@ in some window."
 (autoload 'markdown-mode "markdown-mode"
   "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
 (if (fboundp 'check-parens)
     (add-hook 'markdown-mode-hook
 	      (lambda ()
