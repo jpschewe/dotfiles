4b7cd21c4514f20bacf36d80b88eb745c1ec4ee3
Jon Schewe <jpschewe@mtu.net>
2026-02-07 19:19:23 -0600

    emacs: load embark with use-package

--- emacs/init.el
+++ emacs/init.el
@@ -2042,8 +2042,40 @@ in some window."
 ;; https://github.com/oantolin/embark?tab=readme-ov-file
 ;;
 ;;;;;;;;;;;;
-(require 'embark)
-(global-set-key (kbd "C-.") 'embark-act)
+(use-package embark
+  :ensure t
+
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ;;("C-;" . embark-dwim)        ;; good alternative: M-.
+   ;;("C-h B" . embark-bindings) ;; alternative for `describe-bindings'
+   )
+  
+  :init
+
+  ;; Optionally replace the key help with a completing-read interface
+  ;;(setq prefix-help-command #'embark-prefix-help-command)
+
+  ;; Show the Embark target at point via Eldoc. You may adjust the
+  ;; Eldoc strategy, if you want to see the documentation from
+  ;; multiple providers. Beware that using this can be a little
+  ;; jarring since the message shown in the minibuffer can be more
+  ;; than one line, causing the modeline to move up and down:
+
+  ;; (add-hook 'eldoc-documentation-functions #'embark-eldoc-first-target)
+  ;; (setq eldoc-documentation-strategy #'eldoc-documentation-compose-eagerly)
+
+  ;; Add Embark to the mouse context menu. Also enable `context-menu-mode'.
+  ;; (context-menu-mode 1)
+  ;; (add-hook 'context-menu-functions #'embark-context-menu 100)
+
+  :config
+
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
 
 
 ;;;;;;;;;;;
